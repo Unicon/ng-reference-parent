dc77565fa52477be556180374f65d198a7afa09a
pneff <pneff@unicon.net>
2018-03-26 09:41:55 -0700

    user rest api working with security

---
+++ user-rest-api/src/main/java/com/wiley/wpng/ref/userapi/RestSecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.wiley.wpng.ref.userapi;
+
+import com.wiley.wpng.ref.user.api.filter.JWTAuthorizationFilter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@ConfigurationProperties(prefix = "oidc")
+@EnableWebSecurity
+public class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private Log log = LogFactory.getLog(RestSecurityConfiguration.class);
+
+
+
+    private Map<String, String> oidcIssuers = new HashMap<>();
+
+    public Map<String, String> getOidcIssuers() {
+        return oidcIssuers;
+    }
+
+    public void setOidcIssuers(Map<String, String> oidcIssuers) {
+        this.oidcIssuers = oidcIssuers;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("!!!!!!!!! Issuers = " + getOidcIssuers().toString());
+        if (oidcIssuers != null) {
+            for (String key : oidcIssuers.keySet()) {
+                log.info("key: " + key + " value: " + oidcIssuers.get(key));
+            }
+
+        }
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user/auth").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(jwtAuthorizationFilter())
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/user/auth");
+    }
+
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    private JWTAuthorizationFilter jwtAuthorizationFilter() throws Exception {
+
+
+        JWTAuthorizationFilter filter = new JWTAuthorizationFilter(authenticationManager(), oidcIssuers);
+        return filter;
+
+    }
+}
